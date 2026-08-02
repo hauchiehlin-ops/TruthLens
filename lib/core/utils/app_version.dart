29a4f059af3a5b9175712ec847d27a735b8d6d99
Barret Lin <hauchieh.lin@gmail.com>
2026-08-02 14:57:20 +0800

    fix: update AppVersion fallback version to 1.4.0+5 and sync Windows versioning

--- lib/core/utils/app_version.dart
+++ lib/core/utils/app_version.dart
@@ -2,8 +2,8 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 /// 應用程式版本控制常數（動態讀取系統版號）
 class AppVersion {
-  static String _version = '1.1.0';
-  static String _buildNumber = '3';
+  static String _version = '1.4.0';
+  static String _buildNumber = '5';
 
   /// 版本名稱（版號，例如 1.1.0）
   static String get version => _version;
